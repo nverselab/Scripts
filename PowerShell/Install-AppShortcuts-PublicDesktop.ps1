fb47a8c45e8caf26cc6b353c8a57a6e0b2dacf71
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-06-07 14:27:38 -0500

    Replaced % with ForEach-Object

--- PowerShell/Install-AppShortcuts-PublicDesktop.ps1
+++ PowerShell/Install-AppShortcuts-PublicDesktop.ps1
@@ -1,7 +1,7 @@
 $Array = @(
     @{Target="C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";ShortcutPath="$env:Public\Desktop\Word.lnk"},
     @{Target="C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE";ShortcutPath="$env:Public\Desktop\Excel.lnk"},
-    @{Target="C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";ShortcutPath="$env:Public\Desktop\Outlook.lnk"}) | % { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+    @{Target="C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";ShortcutPath="$env:Public\Desktop\Outlook.lnk"}) | ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
 
 
 $Array.count
