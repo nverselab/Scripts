5adaec25630d9b880f924aa2fac013a092156481
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-06-07 14:24:34 -0500

    Fixed Public Desktop Shortcuts

--- PowerShell/Install-AppShortcuts-PublicDesktop.ps1
+++ PowerShell/Install-AppShortcuts-PublicDesktop.ps1
@@ -1,40 +1,21 @@
-function set-shortcut {
-    param ( [string]$SourceLnk, [string]$DestinationPath )
-        $WshShell = New-Object -comObject WScript.Shell
-        $Shortcut = $WshShell.CreateShortcut($SourceLnk)
-        $Shortcut.TargetPath = $DestinationPath
-        $Shortcut.Save()
-        }
-    
-    $DesktopPath = ([Environment]::GetEnvironmentVariable("Public")) + "\Desktop"
-    
-    $ShortcutPath = "C:\Program Files\Microsoft Office\root\Office16"
-    
-    $ShortcutTarget = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE",
-    "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE",
-    "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
-    
-    if(Test-Path -path $ShortcutPath){
-    
-        write-host "Shortcut already exists.  Skipping."
-    
-        }
-    
-        Else {
-    
-        set-shortcut $ShortcutPath $ShortcutTarget
-    
-        If (Test-Path -path $ShortcutPath){
-    
-            write-host "Shortcut successfully created."
-    
-            }
-    
-            Else {
-            
-                write-host "Shortcut creation failed."
-                exit 1
-    
-                }
-    
-        }
+$Array = @(
+    @{Target="C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";ShortcutPath="$env:Public\Desktop\Word.lnk"},
+    @{Target="C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE";ShortcutPath="$env:Public\Desktop\Excel.lnk"},
+    @{Target="C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";ShortcutPath="$env:Public\Desktop\Outlook.lnk"}) | % { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+
+
+$Array.count
+
+for($i=0; $i -lt $Array.count; $i++){
+
+$TargetFile = $Array[$i].Target
+$ShortcutPath = $Array[$i].ShortcutPath
+
+Write-Host "Creating $TargetFile for $ShortcutPath"
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+ }
