832b651a041b898ec29ed6f21bc3b097817c68d1
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-06-10 12:28:17 -0500

    Added script to install Chrome Enterprise with Master Preferences file for either x86 or 64bit.

---
+++ PowerShell/Install-Chrome-MasterPrefsFile.ps1
@@ -0,0 +1,17 @@
+﻿$arguments = '/I googlechromestandaloneenterprise64v99.msi /qn /norestart'
+
+Start-Process msiexec.exe -ArgumentList $arguments -Wait
+
+if (Test-Path "c:\Program Files\Google\Chrome\Application") {
+
+    Copy-Item "master_preferences" "c:\Program Files\Google\Chrome\Application" -Force -ErrorAction Ignore
+
+}
+
+if (Test-Path "c:\Program Files (x86)\Google\Chrome\Application") {
+
+    Copy-Item "master_preference" "C:\Program Files (x86)\Google\Chrome\" -Force -ErrorAction Ignore
+
+}
+
+exit 0
