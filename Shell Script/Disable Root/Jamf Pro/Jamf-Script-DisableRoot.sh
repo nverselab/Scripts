79ddc8769cc5e7152803e5e5dc4b735ce6bedb93
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-06-15 12:47:10 -0500

    fixed typo

--- Shell Script/Disable Root/Jamf Pro/Jamf-Script-DisableRoot.sh
+++ Shell Script/Disable Root/Jamf Pro/Jamf-Script-DisableRoot.sh
@@ -9,7 +9,7 @@ dscl . delete /Users/root AuthenticationAuthority
 dscl . -create /Users/root Password '*'
 
 # Disable root login by setting root's shell to /usr/bin/false
-## Note: This will prevent use of `su root` or 'su root' or 'su -' to elevate to root.
+## Note: This will prevent use of 'su root' or 'su -' to elevate to root.
 ##       Comment out, remove, or use 'sudo' with another admin account if needed.
 dscl . -create /Users/root UserShell /usr/bin/false
 
