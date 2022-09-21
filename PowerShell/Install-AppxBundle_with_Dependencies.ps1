459f78757165b2694a6133fd5fa9b4ecdbaa2474
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-09-20 20:37:01 -0500

    Added deployment note

--- PowerShell/Install-AppxBundle_with_Dependencies.ps1
+++ PowerShell/Install-AppxBundle_with_Dependencies.ps1
@@ -1,12 +1,14 @@
+## Install .AppxBundle and Dependencies 
+## NOTE: Add-AppxPackage must be run in User Context. If you want to install for all users as System, you'll need to use 'DISM.exe /Online /Add-ProvisionedAppxPackage /PackagePath:$AppxBundle /SkipLicense' and call out each dependency individually with /DependencyPackagePath.
+
 $AppxBundle= Get-ChildItem -Path "$PSScriptRoot" -Filter "*.AppxBundle*" | Select-Object -ExpandProperty FullName # Searches the directory where the script was run for .AppxBundle files
 $Dependencies = Get-ChildItem -Path "$PSScriptRoot\dependencies" -Filter "*.appx*" | Select-Object -ExpandProperty FullName # Include dependency .appx files in a subdirectory labeled "dependencies".  If none needed, remove this line and the -DependencyPath flag from Add-AppxPackage
 
 Add-AppxPackage -Path "$AppxBundle" -DependencyPath $Dependencies
 
-$AppxLookup=$(Get-AppxPackage | Where-Object {$_.Name -eq "$AppxName"})
-
 ## Detection Script
 
+$AppxLookup=$(Get-AppxPackage | Where-Object {$_.Name -eq "$AppxName"})
 $AppxName="Microsoft.CompanyPortal" # This is the expected AppxPackage Name attribute
 
 if (Test-Path $AppxLookup.InstallLocation) {
