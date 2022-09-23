73567e8fc052714fedc8e15039c1ef4876f1cae3
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2022-09-23 15:08:25 -0500

    Simplified if statement

--- PowerShell/Detect-Adobe_Reader_DC.ps1
+++ PowerShell/Detect-Adobe_Reader_DC.ps1
@@ -1,27 +1,16 @@
 $Reader32 = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
 $Reader64 = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 
-If(Test-Path $Reader32) {
+If((Test-Path $Reader32) -or (Test-Path $Reader64)) {
     
-    Write-Host "Reader 32 bit found.  Exiting Successfully."
+    Write-Host "Acrobat Reader DC Found.  Exiting Successfully."
     exit 0
 
 }
 
 Else {
 
-    If (Test-Path $Reader64) {
-
-    Write-Host "Reader 64 bit found. Exiting Successfully."
-    exit 0
-
-    }
-
-    Else { 
-        
-        Write-Host "Reader DC not found.  Exiting Unsuccessfully."
-        exit 1 
-        
-        }
+    Write-Host "Acrobat Reader DC not found.  Exiting Unsuccessfully."
+    exit 1 
 
 }
