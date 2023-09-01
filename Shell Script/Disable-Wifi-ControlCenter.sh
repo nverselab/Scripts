1e71998438d0a5c21cfef222126b39874f15aeb2
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2023-09-01 10:54:47 -0500

    Create Disable-Wifi-ControlCenter.sh

---
+++ Shell Script/Disable-Wifi-ControlCenter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Hide Wifi from Control Center (Must run Per User)
+
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+sudo -u $currentUser defaults -currentHost write com.apple.controlcenter WiFi -int 8
+
+# Optional Disable Wifi Completely (System Setting)
+
+# networksetup -setairportpower en0 off
+
+# networksetup -setnetworkserviceenabled Wi-Fi off
