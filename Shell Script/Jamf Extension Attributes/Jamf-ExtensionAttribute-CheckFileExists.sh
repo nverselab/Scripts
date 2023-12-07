ff9bedd64032b25703f21d9d225db856016480e1
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2023-12-07 13:45:29 -0600

    Update Jamf-ExtensionAttribute-CheckFileExists.sh

--- Shell Script/Jamf Extension Attributes/Jamf-ExtensionAttribute-CheckFileExists.sh
+++ Shell Script/Jamf Extension Attributes/Jamf-ExtensionAttribute-CheckFileExists.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
+##### Use the below for a file
+
 FILE="/Library/ArcticWolfNetworks/Agent/uninstall.sh"
 
 if [ ! -f $FILE ]; then
+    echo "<result>Not Installed</result>"
+else
+    echo "<result>Installed</result>"
+fi
+
+##### Use the below for a folder instead of a file
+
+#!/bin/bash
+FOLDER="/Library/ArcticWolfNetworks/Agent"
+
+if [ ! -d "$FOLDER" ]; then
     echo "<result>Not Installed</result>"
 else
     echo "<result>Installed</result>"
