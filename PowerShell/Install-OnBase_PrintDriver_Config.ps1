758f9e6ee2285bf41cbccf5484639552847d696f
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2024-04-16 10:50:39 -0500

    Update Install-OnBase_PrintDriver_Config.ps1

--- PowerShell/Install-OnBase_PrintDriver_Config.ps1
+++ PowerShell/Install-OnBase_PrintDriver_Config.ps1
@@ -6,7 +6,7 @@ if (!(Test-Path -Path "C:\IntuneLogs")) {
 
     }
 
-Start-Transcript -Path "C:\IntuneLogs\ISE-Posture-Module-Script.log"
+Start-Transcript -Path "C:\IntuneLogs\OnBase-Script.log"
 
 # Variables
 $OnBaseInstaller = "HylandUnityClient.msi"
@@ -15,8 +15,8 @@ $OnBaseConfig = "obunity.exe.config"
 $OnBaseConfigPath = "C:\Program Files (x86)\Hyland\Unity Client\obunity.exe.config"
 
 # Start Install
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$OnBaseInstaller`" /quiet" -Wait
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$PrintDriverInstaller`" /quiet" -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$OnBaseInstaller`" /quiet /l*v `"C:\IntuneLogs\OnBase-Installer.log`"" -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$PrintDriverInstaller`" /quiet /l*v `"C:\IntuneLogs\OnBase-PrintDriver.log`"" -Wait
 
 # Overwrite Config File
 Copy-Item -Path $OnBaseConfig -Destination $OnBaseConfigPath -Force
