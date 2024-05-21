81d3e56e1454aebe197ff63238492996b811c9f4
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2024-05-21 15:24:11 -0500

    Update Jamf-ExtensionAttribute-SNOWAgentVersion.sh

--- Shell Script/Jamf Extension Attributes/Jamf-ExtensionAttribute-SNOWAgentVersion.sh
+++ Shell Script/Jamf Extension Attributes/Jamf-ExtensionAttribute-SNOWAgentVersion.sh
@@ -5,8 +5,12 @@ agentPath="/opt/snow/snowagent"
 
 # Check if the agent exists
 if [ -f "$agentPath" ]; then
+    # If it exists, get the version and store it in a variable
     agentVersion=$($agentPath version)
-    echo "<result>$agentVersion</result>"
+    # Truncate the version to only show characters before the '+'
+    truncatedVersion=$(echo $agentVersion | awk -F'+' '{print $1}')
+    # Echo the truncated version
+    echo "<result>$truncatedVersion</result>"
 else
     # If it doesn't exist, echo "Not Installed"
     echo "<result>Not Installed</result>"
