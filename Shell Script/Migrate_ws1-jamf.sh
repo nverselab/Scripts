b160fd1bb78eafe1b0daf132e914f640ac061d0a
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2024-09-20 11:01:10 -0500

    Update Migrate_ws1-jamf.sh

--- Shell Script/Migrate_ws1-jamf.sh
+++ Shell Script/Migrate_ws1-jamf.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Note: This is a work in progress and is not ready for production.
+# Note: this is a work in progress and may not be ready for production.
 
 originurl="https://xx####.awmdm.com" # The WS1 API URL of source environment
-wsoClientID="required" # REST API token for the source WS1 tenant
-wsoSecret="required" # Base64 encoded API credentials for the source WS1 tenant
+oAuthTokenURL="https://na.uemauth.vmwservices.com/connect/token" # The Access Token URL for your VMWare WS1 Tenant (differs by region: https://kb.omnissa.com/s/article/76967)
+wsoClientID="required" # REST API oAuth client ID for the source WS1 tenant. Requires Console Administrator role or similar to use the API
+wsoSecret="required" # API oAuth credentials for the client ID in the source WS1 tenant
 NewjssURL="https://yourSubDomain.jamfcloud.com" # The URL of the new JSS server (e.g. https://yourSubDomain.jamfcloud.com)
 enrollInvitationID="optional"
 serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
 
+# Obtain an authorization token
+auth_response=$(curl -X POST "$oAuthTokenURL" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "grant_type=client_credentials&client_id=$wsoClientID&client_secret=$wsoSecret")
+
+# Extract the bearer token from the response using sed
+bearertoken=$(echo $auth_response | sed -n 's/.*"access_token":"\([^"]*\)".*/\1/p')
 
 # Function to unenroll device from ws1 by serial number
 # Source: https://github.com/euc-oss/euc-samples/blob/main/UEM-Samples/Utilities%20and%20Tools/macOS/Migration-Tool/migrationToolWS1/payload/Library/Application%20Support/VMware/migrator.sh
@@ -27,7 +35,7 @@ fi
   # API Call to enterprise wipe device
   url="$originurl/api/mdm/devices/commands?command=EnterpriseWipe&searchBy=Serialnumber&id=$serial"
   echo "POST - $url"
-  response=$(/usr/bin/curl -X POST $url -H "Authorization: Bearer $wsoSecret" -H "aw-tenant-code: $wsoClientID" -H "accept: application/json" -H "Content-Type: application/json" -H "Content-Length: 0")
+  response=$(/usr/bin/curl -X POST $url -H "Authorization: Bearer $bearertoken" -H "accept: application/json" -H "Content-Type: application/json" -H "Content-Length: 0")
   #check if successful
   if [[ ! -z "$response" ]]; then
     #api failed
@@ -40,16 +48,35 @@ fi
 # Check if credentials are set
 if [[ "$wsoClientID" == "required" || "$wsoSecret" == "required" ]]; then
   echo "Error: API credentials are not set. Please set wsoClientID and wsoSecret."
-  exit 1
+  #exit 1
 fi
 
 removeWS1
+
+echo "Waiting for all Jamf profiles to be removed..."
+
+# Loop until no profiles with the word "jamf" are found or until 30 minutes have passed
+timeout=1800  # 30 minutes in seconds
+interval=10   # Check every 10 seconds
+elapsed=0
+
+while profiles -P | grep -iqE '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'; do
+  if [ $elapsed -ge $timeout ]; then
+    echo "Timeout reached. WSO profiles were not removed within 30 minutes."
+    exit 1
+  fi
+  echo "WSO profiles still present. Checking again in 10 seconds..."
+  sleep $interval
+  elapsed=$((elapsed + interval))
+done
+
+echo "All WSO profiles have been removed."
 
 ########## Re-Enroll the Computer to new JSS server ##########
 # Instructions: Uncomment one of the desired enrollment workflows and comment out the other two.
 # Check if $enrollInvitationID has a value or is not equal to "optional"
 
-if [ -n "$enrollInvitationID" ] || [ "$enrollInvitationID" != "optional" ]; then
+if [ -n "$enrollInvitationID" ] && [ "$enrollInvitationID" != "optional" ]; then
 
   echo "Enrollment Invitation ID is set. Opening Enrollment Invitation URL."
 
@@ -62,12 +89,20 @@ else
   echo "Enrollment Invitation ID not set. Opening generic Enroll URL."
 
 # Option 2: Generic Enrollment URL (requires Jamf Admin with Enroll Permissions or user is in IdP group allowed to Enroll)
-  # Opens the Enrollment Invitation URL 
-  open $NewjssURL/enroll
+
+  if [ -n "$NewjssURL" ] && [ "$NewjssURL" != "https://yourSubDomain.jamfcloud.com" ]; then
+    # Opens the Enrollment Invitation URL 
+    open $NewjssURL/enroll
+
+  else
+
+    echo "New JSS URL not set. Attempting Automated Device Enrollment with Apple Business/School Manager."
+    
+  fi
 
 fi
 
 # Option 3: Attempt to re-enroll using ADE (Requires correct MDM assignment in ABM/ASM)
-#sudo profiles renew -type enrollment
+sudo profiles renew -type enrollment
 
 exit 0
