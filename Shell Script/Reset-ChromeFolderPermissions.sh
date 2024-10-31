aeef65ffc079fff5f93c4cce491bb73040fc6969
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2024-10-31 12:08:11 -0500

    Update Reset-ChromeFolderPermissions.sh

--- Shell Script/Reset-ChromeFolderPermissions.sh
+++ Shell Script/Reset-ChromeFolderPermissions.sh
@@ -5,8 +5,8 @@ users=$(dscl . list /Users | grep -v '^_' | grep -v 'nobody' | grep -v 'root')
 
 # Loop through each user
 for user in $users; do
-  echo "Checking $user"
   userHome=$(dscl . -read /Users/$user NFSHomeDirectory | awk '{print $2}')
+  echo "Checking $user in $userHome"
   
   # Check if the directory exists, if not, create it
   if [ ! -d "$userHome/Library/Application Support/Google" ]; then
@@ -15,4 +15,5 @@ for user in $users; do
 
   # Change ownership of the Google directory
   sudo chown -R "$user:staff" "$userHome/Library/Application Support/Google"
+  sudo chmod -R 755 "$userHome/Library/Application Support/Google"
 done
