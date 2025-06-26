0520657a08a186d366715633ce92f6e2ef22ac93
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2025-06-26 15:35:51 -0500

    Create Run_Rapid7-InstallScript.sh

---
+++ Shell Script/Run_Rapid7-InstallScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+arch=$(/usr/bin/arch)
+
+if [ "$arch" == "arm64" ]; then
+    echo "Apple Silicon Detected"
+    chmod +x /Users/Shared/CPCStaging/agent_installer-arm64.sh
+    /bin/sh /Users/Shared/CPCStaging/agent_installer-arm64.sh install_start --token $4
+else
+    echo "Intel Detected"
+    chmod +x /Users/Shared/CPCStaging/agent_installer.sh
+    /bin/sh /Users/Shared/CPCStaging/agent_installer.sh install_start --token $4
+fi
+
+# Detect Rapid7 is running
+
+if pgrep -x "ir_agent" >/dev/null; then
+    echo "Install Successful"
+    # Clean Up
+    rm -rf /private/tmp/rapid7
+    exit 0
+else
+    echo "Install Failed"
+    # Clean Up
+    rm -rf /private/tmp/rapid7
+    exit 1
+fi
