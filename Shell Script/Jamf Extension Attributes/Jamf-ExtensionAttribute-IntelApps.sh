b5989e90f7c0942dacb7c6fa202dbcc66e1d8147
Jonathan Nelson <107066080+nverselab@users.noreply.github.com>
2026-06-02 10:36:39 -0500

    Add script to detect installed Intel applications
    This script detects Intel-based applications installed in specified directories and logs the results.

---
+++ Shell Script/Jamf Extension Attributes/Jamf-ExtensionAttribute-IntelApps.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## SOURCE: https://github.com/rtrouton/rtrouton_scripts/blob/main/rtrouton_scripts/detect_installed_intel_based_apps/detect_installed_intel_based_apps.sh
+
+# Detect all Intel apps installed in /Applications, /Library
+# or /usr/local and output list Jamf Pro.
+
+intel_app_logfile="/var/log/intel_apps_installed.log"
+ERROR=0
+
+# this script must be run with root privileges
+if [[ "$(/usr/bin/id -u)" -eq 0 ]]; then
+
+	# Create log file if not present
+	if [[ -f "$intel_app_logfile" ]]; then
+       echo "$intel_app_logfile found. Proceeding..."
+	else   
+       echo "Creating $intel_app_logfile log. Proceeding..."
+       touch "$intel_app_logfile"
+	fi
+
+	# Get a list of all installed applications
+	intel_app_list=$(/usr/sbin/system_profiler SPApplicationsDataType)
+
+	if [[ -n "$intel_app_list" ]]; then
+
+		# get all non-64 Bit applications from the initial list
+		intel_app_list=$(echo "$intel_app_list" | /usr/bin/grep -A3 "Intel")
+
+		# filter out all applications in /Applications, /Library and /usr/local
+		intel_app_list=$(echo "$intel_app_list" | /usr/bin/grep -E "Location:[^/]*/(Applications|Library|usr/local)/")
+
+		# remove everything except the path
+		intel_app_list=$(echo "$intel_app_list" | /usr/bin/sed -n 's/.*Location:[[:space:]]*\(.*\)/\1/p')
+
+		if [[ -n "$intel_app_list" ]]; then
+			echo "<result>$intel_app_list</result>"
+		else
+		    echo "No Intel applications found" > "$intel_app_logfile"
+		fi
+	fi
+
+else
+	log "ERROR! You must be root in order to run this script!"
+	ERROR=1
+fi
+
+exit $ERROR 
